2c1ba114c536add8373744087e42b77612cdbfa8
Alejandro Lopez <alexjes6543@gmail.com>
2023-04-23 21:02:15 -0400

    get movement search endpoint

--- src/movement/movements.controller.ts
+++ src/movement/movements.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Query, Res } from '@nestjs/common';
 
 import { MovementService } from './movements.service';
 
-@Controller('movement')
+@Controller('movements')
 export class MovementController {
     constructor(private movementService: MovementService) { }
     @Get()
     async getAll(@Res() res: any) {
         const movements = await this.movementService.findAll();
+        return res.status(HttpStatus.OK).json(movements);
+    }
+
+    @Get('/search')
+    async getMovementsBy(@Res() res: any, @Query('id') id: number, @Query('user_id') user_id: number, @Query('description') description: string, @Query('amount') amount: number, @Query('type_id') type_id: number, @Query('debt') debt: number, @Query('startDate') startDate: Date, @Query('endDate') endDate: Date) {
+        const movements = await this.movementService.getMovementsBy(id, user_id, description, amount, type_id, debt, startDate, endDate);
         return res.status(HttpStatus.OK).json(movements);
     }
 
--- src/movement/movements.service.ts
+++ src/movement/movements.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, Between, } from 'typeorm';
 
 import { Movements } from './movements.entity';
 
@@ -18,6 +18,16 @@ export class MovementService {
         }
     }
 
+    async getMovementsBy(id: number, user_id: number, description: string, amount: number, type_id: number, debt: number, startDate: Date, endDate: Date): Promise<Movements[]> {
+        try {
+            const query = this.createSearchQuery({ id, user_id, description, amount, type_id, debt, startDate, endDate });
+            return await this.movementRepository.find({
+                where: query
+            });
+        } catch (e) {
+            this.handleDBExceptions(e);
+        }
+    }
 
     private handleDBExceptions(error: any) {
         if (error.code === '23505') throw new BadRequestException(error.detail);
@@ -25,6 +35,18 @@ export class MovementService {
         throw new InternalServerErrorException(
             'Unexpected Error. Check Server Logs',
         );
+    }
+
+    private createSearchQuery(params) {
+        let query = {};
+        if (params.id) query['id'] = params.id;
+        if (params.user_id) query['user_id'] = params.user_id;
+        if (params.description) query['description'] = params.description;
+        if (params.amount) query['amount'] = params.amount;
+        if (params.type_id) query['movement_type_id'] = params.type_id;
+        if (params.debt) query['debt'] = params.debt;
+        if (params.startDate || params.endDate) query['date'] = Between(params.startDate ?? new Date(0), params.endDate ?? new Date('9999-12-31'));
+        return query;
     }
 
 }
